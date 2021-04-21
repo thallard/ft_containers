a5f52eef014a3b285b54cbd028ad296e471fbaa5
thallard <thallard@student.42lyon.fr>
2021-04-21 16:24:10 +0200

    Beaucoup de choses faites

--- includes/List.hpp
+++ includes/List.hpp
@@ -47,7 +47,7 @@ namespace ft
 		Iterator(pointer ptr) : ptr_(ptr){};
 		Iterator &operator=(Iterator const &ref)
 		{
-			ptr_ = ref._ptr;
+			ptr_ = ref.ptr_;
 			return (*this);
 		};
 		self_type operator++()
@@ -280,8 +280,8 @@ namespace ft
 		const_iterator end() const;
 		reverse_iterator rbegin();
 		reverse_iterator rend();
-		const_reverse_iterator crbegin() const;
-		const_reverse_iterator crend() const;
+		const_reverse_iterator rbegin() const;
+		const_reverse_iterator rend() const;
 
 		// Access members
 		reference front();
@@ -296,12 +296,13 @@ namespace ft
 
 		// Modifiers members
 		void clear();
-		// iterator insert(iterator pos, const T &value);
-		// void insert(iterator pos, size_type count, const T &value);
-		// template <class InputIt>
-		// void insert(iterator pos, InputIt first, InputIt last);
-		// iterator erase(iterator pos);
-		// iterator erase(iterator first, iterator last);
+		void assign(size_type n, const value_type &val);
+		void assign(iterator first, iterator last);
+		iterator insert(iterator pos, const T &value);
+		void insert(iterator pos, size_type count, const T &value);
+		void insert(iterator pos, iterator first, iterator last);
+		iterator erase(iterator pos);
+		iterator erase(iterator first, iterator last);
 		void push_back(const T &value);
 		void pop_back();
 		void push_front(const T &value);
@@ -309,6 +310,14 @@ namespace ft
 		void resize(size_type count);
 		void swap(List &other);
 		void reverse();
+		void remove(const T &value);
+
+		template <class UnaryPredicate>
+		void remove_if(UnaryPredicate p);
+		void unique();
+		template <class BinaryPredicate>
+		void unique (BinaryPredicate binary_pred);
+		void sort();
 	};
 
 	// Default constructor
@@ -322,23 +331,30 @@ namespace ft
 
 	// Range constructor
 	template <class T, class Alloc>
-	List<T, Alloc>::List(List<T, Alloc>::iterator first, List<T, Alloc>::iterator last)
+	List<T, Alloc>::List(List<T, Alloc>::iterator first, List<T, Alloc>::iterator last) : _size(0)
 	{
-		int size = 0;
-
-		while (first != last)
+		List<T, Alloc>::iterator copy = first;
+		List<T, Alloc>::iterator it = first;
+		while (copy != last)
 		{
-			size++;
-			first++;
+			copy++;
+			_size++;
 		}
-		_size = size;
-		_nodes = reinterpret_cast<Element<T> *>(allocator_type::allocate(_size));
-		// Malloc previous tout remplir avec val et next et prev
-		while (first != last)
+		_nodes = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
+		Element<T> *tmp = _nodes;
+		Element<T> save;
+		while (it != last)
 		{
-
-			first++;
+			tmp->_next = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
+			save = *it;
+			tmp->_content = save._content;
+			tmp->_next->_prev = tmp;
+			tmp = tmp->_next;
+			it++;
 		}
+		tmp->_content = static_cast<T>(_size);
+		tmp->_next = _nodes;
+		_nodes->_prev = tmp;
 	}
 
 	// Fill constructor
@@ -347,20 +363,16 @@ namespace ft
 	{
 		_nodes = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
 		Element<T> *tmp = _nodes;
-		_nodes->_content = static_cast<T>(size);
-		tmp->_next = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
-		tmp = tmp->_next;
-		tmp->_prev = _nodes;
 		for (unsigned int i = 0; i < size; i++)
 		{
-			// Malloc previous tout remplir avec val et next et prev
 			tmp->_next = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
 			tmp->_content = val;
 			tmp->_next->_prev = tmp;
 			tmp = tmp->_next;
 		}
 		tmp->_content = static_cast<T>(size);
-		tmp->_next = NULL;
+		tmp->_next = _nodes;
+		_nodes->_prev = tmp;
 	}
 
 	// Overload operator=
@@ -371,7 +383,6 @@ namespace ft
 			return *this;
 		_size = ref._size;
 		_count = ref._count;
-		// for (unsigned int i = 0; i <)
 		_nodes = ref._nodes;
 		return *this;
 	}
@@ -384,14 +395,14 @@ namespace ft
 		Element<T> *elem;
 
 		elem = _nodes;
-		for (unsigned int i = 0; i < _size + 2; i++)
+		for (unsigned int i = 0; i < _size + 1; i++)
 		{
 			next = elem->_next;
-			if (next)
+			if (i + 1 < _size + 1)
 				elem = next;
 			else
 				this->deallocate(reinterpret_cast<int *>(elem), sizeof(Element<T> *));
-			if (next)
+			if (i + 1 < _size + 1)
 				this->deallocate(reinterpret_cast<int *>(next->_prev), sizeof(Element<T> *));
 			next = NULL;
 		}
@@ -402,13 +413,13 @@ namespace ft
 	template <class T, class Alloc>
 	typename List<T, Alloc>::reference List<T, Alloc>::front()
 	{
-		return (_nodes->_next->_content);
+		return (_nodes->_content);
 	}
 	// Front access const member
 	template <class T, class Alloc>
 	typename List<T, Alloc>::const_reference List<T, Alloc>::front() const
 	{
-		return (_nodes->_next->_content);
+		return (_nodes->_content);
 	}
 
 	// Back access member
@@ -451,23 +462,27 @@ namespace ft
 		return std::min<size_type>(allocator_type::max_size(), static_cast<size_type>(std::numeric_limits<difference_type>::max())) / sizeof(size_type);
 	}
 
-	// Members access
-	template <class T, class Alloc>
-	void List<T, Alloc>::clear()
-	{
-		for (unsigned int i = 0; i < _size; i++)
-			;
-	}
-
 	// Members access push_back
 	template <class T, class Alloc>
 	void List<T, Alloc>::push_back(const T &value)
 	{
 		resize(_size + 1);
-		Element<T> *tmp = _nodes->_next;
-		for (size_t i = 1; i < _size; i++)
-			tmp = tmp->_next;
+		Element<T> *tmp = _nodes->_prev->_prev;
 		tmp->_content = value;
+		_count++;
+	}
+
+	template <class T, class Alloc>
+	void List<T, Alloc>::pop_back()
+	{
+		if (!_size)
+			return;
+		Element<T> *last = _nodes->_prev->_prev;
+		last->_prev->_next = last->_next;
+		last->_next->_prev = last->_prev;
+		this->deallocate(reinterpret_cast<int *>(last), sizeof(Element<T> *));
+		_size--;
+		_count--;
 	}
 
 	// Members access push_front
@@ -475,48 +490,57 @@ namespace ft
 	void List<T, Alloc>::push_front(const T &value)
 	{
 		resize(_size + 1);
-		Element<T> *tmp = _nodes->_next;
-		for (size_t i = 1; i < _size; i++)
-			tmp = tmp->_next;
-		tmp->_content = value;
-		tmp->_prev->_next = tmp->_next;
-		tmp->_next->_prev = tmp->_prev;
-		tmp->_next = _nodes->_next;
-		tmp->_next->_prev = tmp;
-		_nodes->_next = tmp;
-		tmp->_prev = _nodes;
+		Element<T> *elem = _nodes->_prev->_prev;
+		Element<T> *last = elem->_prev;
+		;
+		Element<T> *end = _nodes->_prev;
+		elem->_content = value;
+		last->_next = end;
+		end->_prev = last;
+		end->_next = elem;
+		elem->_prev = end;
+		elem->_next = _nodes;
+		_nodes->_prev = elem;
+		_nodes = elem;
 	}
 
-		// Members access reverse
+	template <class T, class Alloc>
+	void List<T, Alloc>::pop_front()
+	{
+		if (!_size)
+			return;
+		Element<T> *first = _nodes;
+		_nodes->_prev->_next = _nodes->_next;
+		_nodes->_next->_prev = _nodes->_prev;
+		_nodes = _nodes->_next;
+		this->deallocate(reinterpret_cast<int *>(first), sizeof(Element<T> *));
+		_size--;
+		_count--;
+	}
+
+	// Members access reverse
 	template <class T, class Alloc>
 	void List<T, Alloc>::reverse()
 	{
-		T swap;
-		Element<T> *first = _nodes->_next;
-		Element<T> *last = _nodes->_next;
-		for (size_t i = 1; i < _size; i++)
+		/*Element<T> *first = _nodes->_next;
+		Element<T> *last = _nodes;
+		for (size_t i = 0; i < _size; i++)
 			last = last->_next;
-		for (size_t i = 0; i < _size / 2; i++)
+		for (size_t i = 0; i < _size; i++)
 		{
-			swap = first->_content;
-			first->_content = last->_content;
-			last->_content = swap;
-			last = last->_prev;
-			first = first->_next;
-		}
+			
+		}*/
 	}
 
-	// Members access resize
+	// Modifiers resize
 	template <class T, class Alloc>
 	void List<T, Alloc>::resize(size_type count)
 	{
 		if (_size < count)
 		{
 			Element<T> *tmp = _nodes;
-
-			for (size_t i = 0; i < _size; i++)
-				tmp = tmp->_next;
-			Element<T> *end = tmp->_next;
+			Element<T> *end = tmp->_prev;
+			tmp = end->_prev;
 			for (size_t i = 0; i < count - _size; i++)
 			{
 				tmp->_next = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
@@ -527,14 +551,13 @@ namespace ft
 			tmp->_next = end;
 			end->_prev = tmp;
 			end->_content = static_cast<T>(count);
-			_nodes->_content = static_cast<T>(count);
 			_size = count;
 		}
 		else
 		{
 			Element<T> *tmp = _nodes;
 			size_t i;
-			for (i = 0; i < count; i++)
+			for (i = 1; i < count; i++)
 				tmp = tmp->_next;
 			Element<T> *last = tmp;
 			tmp = tmp->_next;
@@ -545,12 +568,251 @@ namespace ft
 			}
 			Element<T> *end = tmp->_next;
 			this->deallocate(reinterpret_cast<int *>(tmp), sizeof(Element<T> *));
-			last->_next = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
 			last->_next = end;
 			end->_prev = last;
 			end->_content = static_cast<T>(count);
-			_nodes->_content = static_cast<T>(count);
 			_size = count;
+			_count = count;
+		}
+	}
+
+	// Modifiers erase
+	template <class T, class Alloc>
+	typename List<T, Alloc>::iterator List<T, Alloc>::erase(List<T, Alloc>::iterator pos)
+	{
+		List<T, Alloc>::iterator start = begin();
+		Element<T> *tmp = _nodes;
+		while (start != pos)
+		{
+			tmp = tmp->_next;
+			start++;
+		}
+		tmp->_prev->_next = tmp->_next;
+		tmp->_next->_prev = tmp->_prev;
+		this->deallocate(reinterpret_cast<int *>(tmp), sizeof(Element<T> *));
+		_size--;
+		_count--;
+		return start;
+	}
+
+	// Modifiers erase range
+	template <class T, class Alloc>
+	typename List<T, Alloc>::iterator List<T, Alloc>::erase(List<T, Alloc>::iterator first, List<T, Alloc>::iterator last)
+	{
+		List<T, Alloc>::iterator start = first;
+		List<T, Alloc>::iterator begin = this->begin();
+		Element<T> *save = _nodes;
+		while (begin != first)
+		{
+			begin++;
+			save = save->_next;
+		}
+		Element<T> *tmp = save->_next;
+		while (start != last && _size-- && _count--)
+		{
+			tmp = tmp->_next;
+			this->deallocate(reinterpret_cast<int *>(tmp->_prev), sizeof(Element<T> *));
+			start++;
+		}
+		save->_next = tmp;
+		tmp->_prev = save;
+		return start;
+	}
+
+	// Modifiers insert
+	template <class T, class Alloc>
+	typename List<T, Alloc>::iterator List<T, Alloc>::insert(List<T, Alloc>::iterator pos, const T &value)
+	{
+		List<T, Alloc>::iterator copy = pos;
+		List<T, Alloc>::iterator start = this->begin();
+		Element<T> *tmp = _nodes;
+		while (start != copy)
+		{
+			tmp = tmp->_next;
+			start++;
+		}
+		Element<T> *save = tmp->_prev;
+		if (save != _nodes->_prev)
+		{
+			Element<T> *elem = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
+			elem->_content = value;
+			tmp->_prev = elem;
+			save->_next = elem;
+			elem->_prev = save;
+			elem->_next = tmp;
+			_size++;
+			_count++;
+		}
+		else
+			push_front(value);
+		return (++copy);
+	}
+
+	// Modifiers assign
+	template <class T, class Alloc>
+	void List<T, Alloc>::assign(size_type n, const value_type &val)
+	{
+		if (n > _size)
+		{
+			size_t save = _size;
+			this->resize(n);
+			(void)val;
+			Element<T> *tmp = _nodes->_prev->_prev;
+			for (size_t i = save; i < n; i++)
+			{
+				Element<T> *elem = reinterpret_cast<Element<T> *>(this->allocate(sizeof(Element<T> *)));
+				elem->_content = val;
+				tmp->_next = elem;
+				elem->_prev = tmp->_prev;
+				tmp = tmp->_next;
+			}
+		}
+			
+	}
+
+	template <class T, class Alloc>
+	void List<T, Alloc>::clear()
+	{
+		Element<T> *tmp = _nodes;
+		Element<T> *end = _nodes->_prev;
+		while (tmp != end)
+		{
+			tmp = tmp->_next;
+			this->deallocate(reinterpret_cast<int *>(tmp->_prev), sizeof(Element<T> *));
+		}
+		_nodes = tmp;
+		_count = 0;
+		_size = 0;
+	}
+
+	template <class T, class Alloc>
+	void List<T, Alloc>::remove(const T &value)
+	{
+		Element<T> *tmp = _nodes;
+		Element<T> *end = _nodes->_prev;
+
+		while (tmp != end)
+		{
+			if (tmp->_content == value)
+			{
+				tmp->_prev->_next = tmp->_next;
+				tmp->_next->_prev = tmp->_prev;
+				Element<T> *to_delete = tmp;
+				tmp = tmp->_next;
+				this->deallocate(reinterpret_cast<int *>(to_delete), sizeof(Element<T> *));
+				_count--;
+				_size--;
+			}
+			else
+				tmp = tmp->_next;
+		}
+	}
+
+	template <class T, class Alloc>
+	template <class UnaryPredicate>
+	void List<T, Alloc>::remove_if(UnaryPredicate p)
+	{
+		Element<T> *tmp = _nodes;
+		Element<T> *end = _nodes->_prev;
+
+		while (tmp != end)
+		{
+			if (p(tmp->_content))
+			{
+				tmp->_prev->_next = tmp->_next;
+				tmp->_next->_prev = tmp->_prev;
+				Element<T> *to_delete = tmp;
+				tmp = tmp->_next;
+				if (to_delete == _nodes)
+					_nodes = tmp;
+				this->deallocate(reinterpret_cast<int *>(to_delete), sizeof(Element<T> *));
+				_count--;
+				_size--;
+			}
+			else
+				tmp = tmp->_next;
+		}
+	}
+
+	template <class T, class Alloc>
+	void List<T, Alloc>::unique()
+	{
+		Element<T> *tmp = _nodes;
+		Element<T> *end = _nodes->_prev;
+
+		while (tmp != end)
+		{
+			Element<T> *begin = tmp->_next;
+			while (begin != end && begin->_content == tmp->_content)
+			{
+				tmp->_next = begin->_next;
+				begin->_next->_prev = tmp;
+				Element<T> *to_delete = begin;
+				begin = begin->_next;
+				this->deallocate(reinterpret_cast<int *>(to_delete), sizeof(Element<T> *));
+				_count--;
+				_size--;
+			}
+			tmp = tmp->_next;
+		}
+	}
+
+	template <class T, class Alloc>
+	template <class BinaryPredicate>
+	void List<T, Alloc>::unique(BinaryPredicate p)
+	{
+		Element<T> *tmp = _nodes;
+		Element<T> *end = _nodes->_prev;
+
+		while (tmp != end)
+		{
+			Element<T> *begin = tmp->_next;
+			while (begin != end && p(begin->_content,tmp->_content))
+			{
+				tmp->_next = begin->_next;
+				begin->_next->_prev = tmp;
+				Element<T> *to_delete = begin;
+				begin = begin->_next;
+				this->deallocate(reinterpret_cast<int *>(to_delete), sizeof(Element<T> *));
+				_count--;
+				_size--;
+			}
+			tmp = tmp->_next;
+		}
+	}
+
+	template <class T, class Alloc>
+	void  List<T, Alloc>::sort()
+	{
+		Element<T> *tmp = _nodes;
+		Element<T> *end = _nodes->_prev;
+
+		while (tmp != end)
+		{
+			Element<T> *begin = _nodes;
+			while (begin != end)
+			{
+				if (begin != tmp && begin->_content < tmp->_content)
+				{
+					Element<T> *begin_prev = begin->_prev;
+					Element<T> *tmp_next = tmp->_next;
+					Element<T> *begin_next = begin->_next;
+					Element<T> *tmp_prev = tmp->_prev;
+
+					begin_prev->_next = tmp;
+					tmp->_prev = begin_prev;
+					tmp->_next = begin_next;
+					begin_next->_prev = tmp;
+					tmp_prev->_next = begin;
+					begin->_prev = tmp_prev;
+					begin->_next = tmp_next;
+					tmp_next->_prev = begin;
+					begin = _nodes;
+				}
+				else
+					begin = begin->_next;
+			}
+			tmp = tmp->_next;
 		}
 	}
 
@@ -558,16 +820,13 @@ namespace ft
 	template <class T, class Alloc>
 	typename List<T, Alloc>::iterator List<T, Alloc>::begin()
 	{
-		return Iterator<Element<T> >(_nodes->_next);
+		return Iterator<Element<T> >(_nodes);
 	}
 
 	template <class T, class Alloc>
 	typename List<T, Alloc>::iterator List<T, Alloc>::end()
 	{
-		Element<T> *tmp = _nodes->_next;
-		for (size_t i = 1; i <= _size; i++)
-			tmp = tmp->_next;
-		return Iterator<Element<T> >(tmp);
+		return Iterator<Element<T> >(_nodes->_prev);
 	}
 
 	template <class T, class Alloc>
@@ -579,19 +838,13 @@ namespace ft
 	template <class T, class Alloc>
 	typename List<T, Alloc>::const_iterator List<T, Alloc>::end() const
 	{
-		Element<T> *tmp = _nodes->_next;
-		for (size_t i = 1; i < _size; i++)
-			tmp = tmp->_next;
-		return Const_Iterator<Element<T> >(tmp);
+		return Const_Iterator<Element<T> >(_nodes);
 	}
 
 	template <class T, class Alloc>
 	typename List<T, Alloc>::reverse_iterator List<T, Alloc>::rbegin()
 	{
-		Element<T> *tmp = _nodes->_next;
-		for (size_t i = 1; i < _size; i++)
-			tmp = tmp->_next;
-		return Reverse_Iterator<Element<T> >(tmp);
+		return Reverse_Iterator<Element<T> >(_nodes->_prev);
 	}
 
 	template <class T, class Alloc>
@@ -601,19 +854,18 @@ namespace ft
 	}
 
 	template <class T, class Alloc>
-	typename List<T, Alloc>::const_reverse_iterator List<T, Alloc>::crbegin() const
+	typename List<T, Alloc>::const_reverse_iterator List<T, Alloc>::rbegin() const
 	{
-		Element<T> *tmp = _nodes->_next;
-		for (size_t i = 1; i < _size; i++)
-			tmp = tmp->_next;
-		return Const_Reverse_Iterator<Element<T> >(tmp);
+		return Const_Reverse_Iterator<Element<T> >(_nodes->_prev);
 	}
 
 	template <class T, class Alloc>
-	typename List<T, Alloc>::const_reverse_iterator List<T, Alloc>::crend() const
+	typename List<T, Alloc>::const_reverse_iterator List<T, Alloc>::rend() const
 	{
 		return Const_Reverse_Iterator<Element<T> >(_nodes);
 	}
+
+
 }
 
 template <typename T>
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,11 @@
 #include "List.hpp"
 #include <list>
 #include <iterator>
+
+bool not_single_digit (const int& value) { return (value>9); }
+
+bool abso_equal(const int &a, const int &b) { return (a == b ? true : a == -b ? true : false); }
+
 int main()
 {
 	ft::List<int> newList(4, 100);
@@ -9,30 +14,44 @@ int main()
 	std::cout<< "size: " << newList.size() << " -- max_size: " << newList.max_size() << std::endl;
 	
 std::cout << "\n\n";
-	std::cout << "begin: "<< *(--newList.begin()) << std::endl;
 
 
-	ft::List<int>::iterator it = newList.begin();
-	ft::List<int>::iterator itend = newList.end();
+
 
 	newList.push_front(999);
 
 	newList.push_back(666);
-
+	//newList.pop_back();
+	//newList.pop_front();
+	std::cout << "\e[35mSize depuis begin de fill: "<< *(--newList.begin()) << std::endl;
 	//newList.reverse();
 	
+	ft::List<int>::iterator it = newList.begin();
+	ft::List<int>::iterator itend = newList.end();
+	ft::List<int> range(it, itend);
 	while (it != itend)
 	{
-		std::cout << *it << std::endl;
+		std::cout << "\e[35mContent : " << *it  << "\e[0m" <<std::endl;
 		it++;
 	}
-	std::cout << "end: " << *newList.end() << std::endl;
+		std::cout << "\e[35mEnd de la list fill : " << *newList.end() << "\e[0m\n";
 
 
-	std::cout << "\nReverse_Iterator: " << std::endl;
-	ft::List<int>::reverse_iterator roui = newList.rbegin();
-	ft::List<int>::reverse_iterator rbase = roui.base();
-	std::cout << "roui: " <<  *roui << " rbase: " << *rbase << std::endl;
+	ft::List<int>::iterator ite = range.begin();
+	ft::List<int>::iterator itende = range.end();
+	std::cout << "\e[34mSize depuis de la list range : " << *(--range.begin()) << "\e[0m\n";
+	while (ite != itende)
+	{
+		std::cout << "\e[34mContent : " << *ite  << "\e[0m" <<std::endl;
+		ite++;
+	}
+	std::cout << "\e[34mEnd de la list range : " << *range.end() << "\e[0m\n";
+
+
+	// std::cout << "\nReverse_Iterator: " << std::endl;
+	// ft::List<int>::reverse_iterator roui = newList.rbegin();
+	// ft::List<int>::reverse_iterator rbase = roui.base();
+	// std::cout << "roui: " <<  *roui << " rbase: " << *rbase << std::endl;
 
 	std::cout << newList.front() << " " << newList.back() << std::endl;
 
@@ -43,16 +62,17 @@ std::cout << "\n\n";
 
 	std::cout<< "size: " << realList.size() << " -- max_size: " << realList.max_size() << std::endl;
 
-	std::cout << *(--realList.begin()) << std::endl;
+	// std::cout << "le end " << *(++(++(++(realList.end())))) << std::endl;
 
 
-	std::list<int>::iterator oui = realList.begin();
-	std::list<int>::iterator non = realList.end();
+
 	realList.push_front(999);
 	realList.push_back(666);
-	realList.reverse();
+	std::list<int>::iterator oui = realList.begin();
+	std::list<int>::iterator non = realList.end();
+	//realList.reverse();
 
-
+std::cout <<"begin de la real :" << *(--realList.begin()) << std::endl;
 
 	
 	while (oui != non)
@@ -60,8 +80,142 @@ std::cout << "\n\n";
 		std::cout << *oui << std::endl;
 		oui++;
 	}
+	std::cout <<"bite" << *oui << std::endl;
 	std::cout << *realList.end() << std::endl;
 	// std::list<int>::reverse_iterator rnon = realList.rbegin();
 	// std::list<int>::reverse_iterator rnon2 = rnon[2];
 	// std::cout << *rnon2 << std::endl;
+
+
+	std::cout << "\n\n\e[96mERASE TEST OMG\n\n";
+
+
+	ft::List<int> list1(5, 10);
+	std::list<int> list2(5, 10);
+
+
+
+	list1.erase(--(--list1.end()));
+	list2.erase(--(--list2.end()));
+
+	ft::List<int>::iterator it10 = list1.begin();
+	ft::List<int>::iterator it20 = list1.end();
+	//std::cout << *it20 << std::endl;
+	while (it10 != it20)
+	{
+		std::cout << "\e[34mContent : " << *it10  << "\e[0m" <<std::endl;
+		it10++;
+	}
+	
+	std::list<int>::iterator it1 = list2.begin();
+	std::list<int>::iterator it2 = list2.end();
+	while (it1 != it2)
+	{
+		std::cout << "\e[35mContent : " << *it1  << "\e[0m" <<std::endl;
+		it1++;
+	}
+	ft::List<int>::iterator fake_begin = list1.begin();
+	ft::List<int>::iterator fake_end = list1.end();
+
+	list1.erase(++(++list1.begin()), (--list1.end()));
+
+	std::list<int>::iterator real_begin = list2.begin();
+	std::list<int>::iterator real_end = list2.end();
+	list2.erase(++(++list2.begin()), (--list2.end()));
+		
+	while (fake_begin != fake_end)
+	{
+		std::cout << "\e[91mContent: " << *fake_begin << std::endl;
+		fake_begin++;
+	}
+	while (real_begin != real_end)
+	{
+		std::cout << "\e[92mContent: " << *real_begin << std::endl;
+		real_begin++;
+	}
+
+	ft::List<int> lst1(3, 10);
+	std::list<int> lst2(3, 10);
+
+	ft::List<int>::iterator it4 = lst1.end();
+	lst1.insert(lst1.end(), 45);
+	ft::List<int>::iterator it3 = lst1.begin();
+	while (it3 != it4)
+	{
+		std::cout << "\e[35mContent : " << *it3  << "\e[0m" <<std::endl;
+		it3++;
+	}
+
+	std::cout << "Real Clear" << std::endl;
+	real_end = lst2.end();
+
+	lst2.clear();
+
+	real_begin = lst2.begin();
+	if (real_begin == real_end)
+		std::cout << "\e[92mNo content left\e[0m" << std::endl;
+	while (real_begin != real_end)
+	{
+		std::cout << "\e[35mContent : " << *real_begin  << "\e[0m" <<std::endl;
+		real_begin++;
+	}
+
+	std::cout << "Our Clear" << std::endl;
+	fake_end = lst1.end();
+
+	lst1.clear();
+
+	fake_begin = lst1.begin();
+
+
+	if (fake_begin == fake_end)
+		std::cout << "\e[92mNo content left\e[0m" << std::endl;
+	while (fake_begin != fake_end)
+	{
+		std::cout << "\e[35mContent : " << *fake_begin  << "\e[0m" <<std::endl;
+		fake_begin++;
+	}
+	std::cout << "\e[0m";
+
+
+	std::cout << "Real Remove" << std::endl;
+	std::list<int> lst4(5, 10);
+	lst4.push_front(-5);
+	lst4.push_front(-5);
+	lst4.push_front(5);
+	lst4.push_front(60);
+	lst4.push_back(7);
+	lst4.unique();
+	lst4.unique(abso_equal);
+	lst4.remove(10);
+	lst4.remove_if(not_single_digit);
+	lst4.push_front(9);
+	lst4.sort();
+	real_end = lst4.end();
+	real_begin = lst4.begin();
+	while (real_begin != real_end)
+	{
+		std::cout << "\e[35mContent : " << *real_begin  << "\e[0m" <<std::endl;
+		real_begin++;
+	}
+	std::cout << "Our Remove" << std::endl;
+	ft::List<int> lst3(5, 10);
+	lst3.push_front(-5);
+	lst3.push_front(-5);
+	lst3.push_front(5);
+	lst3.push_front(60);
+	lst3.push_back(7);
+	lst3.unique();
+	lst3.unique(abso_equal);
+	lst3.remove(10);
+	lst3.remove_if(not_single_digit);
+	lst3.push_front(9);
+	lst3.sort();
+	fake_end = lst3.end();
+	fake_begin = lst3.begin();
+	while (fake_begin != fake_end)
+	{
+		std::cout << "\e[35mContent : " << *fake_begin  << "\e[0m" <<std::endl;
+		fake_begin++;
+	}
 }
